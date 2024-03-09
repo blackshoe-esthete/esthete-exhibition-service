dce5ae15535233578c7c9babf43334edbe10459f
jaehui327 <jaeheekim970327@gmail.com>
2024-03-09 15:29:47 +0900

    Init: esthete exhibition service

---
+++ src/main/java/com/blackshoe/esthete/EstheteExhibitionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blackshoe.esthete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstheteExhibitionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstheteExhibitionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackshoe/esthete/EstheteExhibitionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackshoe.esthete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstheteExhibitionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
