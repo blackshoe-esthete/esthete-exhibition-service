da75cd0e03f4873f8f397a3e799fee7fec62c9e2
bbbang105 <2018111366@dgu.ac.kr>
2024-04-16 03:23:55 +0900

    [EST-101] Feat : TemporaryExhibition 엔터티 추가

---
+++ src/main/java/com/blackshoe/esthete/entity/TemporaryExhibition.java
@@ -0,0 +1,41 @@
+package com.blackshoe.esthete.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Table(name = "temporary_exhibitions")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TemporaryExhibition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "temporary_exhibitions_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "users_id", foreignKey = @ForeignKey(name = "temporary_exhibitions_fk_users_id"))
+    private User user;
+
+    @Column(name = "exhibitions_uuid", nullable = false)
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private UUID uuid;
+
+    @Column(name = "thumbnail_url", nullable = false)
+    private String cloudfrontUrl;
+
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+}
