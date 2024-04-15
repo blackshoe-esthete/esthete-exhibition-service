4f69e2eb5ab6dfd75097629ad28826f26dfb598f
bbbang105 <2018111366@dgu.ac.kr>
2024-04-16 03:22:59 +0900

    [EST-101] Feat : Photo 엔터티 추가

---
+++ src/main/java/com/blackshoe/esthete/entity/Photo.java
@@ -0,0 +1,56 @@
+package com.blackshoe.esthete.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Table(name = "photos")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "photos_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exhibitions_id", foreignKey = @ForeignKey(name = "photos_fk_exhibitions_id"))
+    private Exhibition exhibition;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "users_id", foreignKey = @ForeignKey(name = "photos_fk_users_id"))
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "temporary_exhibitions_id", foreignKey = @ForeignKey(name = "photos_fk_temporary_exhibitions_id"))
+    private TemporaryExhibition temporaryExhibition;
+
+    @Column(name = "photos_uuid", nullable = false)
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private UUID uuid;
+
+    @Column(name = "view_count")
+    private Long viewCount;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, length = 20)
+    private LocalDateTime createdAt;
+
+    @Column(name = "shooting_time", nullable = false, length = 20)
+    private LocalDateTime shootingTime;
+
+    public void increaseViewCount() {
+        this.viewCount++;
+    }
+}
