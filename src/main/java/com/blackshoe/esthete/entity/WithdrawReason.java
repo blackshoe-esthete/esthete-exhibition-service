91f23c80443d308e132e37f654e3af390c344715
bbbang105 <2018111366@dgu.ac.kr>
2024-04-16 03:23:39 +0900

    [EST-101] Feat : WithdrawReason 엔터티 추가

---
+++ src/main/java/com/blackshoe/esthete/entity/WithdrawReason.java
@@ -0,0 +1,20 @@
+package com.blackshoe.esthete.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "withdraw_reasons")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WithdrawReason {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "withdraw_reasons_id")
+    private Long id;
+
+    @Column(name = "reason", columnDefinition = "TEXT", nullable = false)
+    private String reason;
+}
