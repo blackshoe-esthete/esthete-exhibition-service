e4b7b300326b4affc5261685a948505dafadb72e
bbbang105 <2018111366@dgu.ac.kr>
2024-04-16 03:22:51 +0900

    [EST-101] Feat : Like 엔터티 추가

---
+++ src/main/java/com/blackshoe/esthete/entity/Like.java
@@ -0,0 +1,45 @@
+package com.blackshoe.esthete.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Table(name = "likes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "likes_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exhibitions_id", foreignKey = @ForeignKey(name = "likes_fk_exhibitions_id"))
+    private Exhibition exhibition;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "users_id", foreignKey = @ForeignKey(name = "likes_fk_users_id"))
+    private User user;
+
+    @Column(name = "likes_uuid", nullable = false)
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private UUID uuid;
+
+    @Column(columnDefinition = "BINARY(16)", name = "like_user_uuid", nullable = false)
+    private UUID likeUserUuid;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, length = 20)
+    private LocalDateTime createdAt;
+}
