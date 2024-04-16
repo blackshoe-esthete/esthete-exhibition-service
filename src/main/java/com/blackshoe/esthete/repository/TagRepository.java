c8b14523b281002ff46240aa68d07f414c928b41
bbbang105 <2018111366@dgu.ac.kr>
2024-04-17 03:07:07 +0900

    [EST-98] Feat : 사용자 선호 태그 수정 API 구현 (PUT)

--- src/main/java/com/blackshoe/esthete/repository/TagRepository.java
+++ src/main/java/com/blackshoe/esthete/repository/TagRepository.java
@@ -2,8 +2,13 @@ package com.blackshoe.esthete.repository;
 
 import com.blackshoe.esthete.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag,Long> {
+    @Query("SELECT u FROM Tag u WHERE u.name = :name")
+    Optional<Tag> findByName(String name);
 }
--- src/main/java/com/blackshoe/esthete/repository/UserRepository.java
+++ src/main/java/com/blackshoe/esthete/repository/UserRepository.java
@@ -2,8 +2,14 @@ package com.blackshoe.esthete.repository;
 
 import com.blackshoe.esthete.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
-public interface UserRepository extends JpaRepository<User,Long>  {
+public interface UserRepository extends JpaRepository<User,Long> {
+    @Query("SELECT u FROM User u WHERE u.userId = :userId")
+    Optional<User> findByUserId(UUID userId);
 }
