8caa5714445b26091c3c813836a702384a80c74e
bbbang105 <2018111366@dgu.ac.kr>
2024-04-17 02:59:14 +0900

    [EST-98] Style : Jwt 토큰 파싱 확인용 로그 추가

--- src/main/java/com/blackshoe/esthete/service/JwtServiceImpl.java
+++ src/main/java/com/blackshoe/esthete/service/JwtServiceImpl.java
@@ -1,5 +1,6 @@
 package com.blackshoe.esthete.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import io.jsonwebtoken.*;
@@ -7,22 +8,25 @@ import io.jsonwebtoken.*;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class JwtServiceImpl implements JwtService {
 
     @Value("${spring.jwt.secret}")
     private String SECRET_KEY;
 
     @Override
-    public UUID extractUserId(String token) throws SignatureException{
+    public UUID extractUserId(String token) throws SignatureException {
         try {
             token = token.replace("Bearer ", "");
 
             Claims claims = Jwts.parser()
-                    .setSigningKey(SECRET_KEY.getBytes())
+                    .setSigningKey(SECRET_KEY)
                     .parseClaimsJws(token)
                     .getBody();
 
             String userId = claims.get("userId", String.class);
+            log.info("UUID : " + userId);
+
             return UUID.fromString(userId);
 
         } catch (JwtException e) {
