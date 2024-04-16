751072db68dc4e63315de224fd56d5a35e9b0502
jaehui327 <jaeheekim970327@gmail.com>
2024-04-16 17:09:39 +0900

    [EST-39] Mdify: S3Config - AmazonS3 -> AmazonS3Client

--- src/main/java/com/blackshoe/esthete/config/S3Config.java
+++ src/main/java/com/blackshoe/esthete/config/S3Config.java
@@ -4,6 +4,7 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -22,10 +23,10 @@ public class S3Config {
     private String region;
 
     @Bean
-    public AmazonS3 amazonS3Client() {
+    public AmazonS3Client amazonS3Client() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
-        return AmazonS3ClientBuilder
+        return (AmazonS3Client) AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region)
