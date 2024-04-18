7adae616389ef11dd94737dfdf456ecac6d3b160
bbbang105 <2018111366@dgu.ac.kr>
2024-04-18 19:20:42 +0900

    [EST-113] Fix : 테스트 에러 방지

--- src/test/java/com/blackshoe/esthete/EstheteExhibitionServiceApplicationTests.java
+++ src/test/java/com/blackshoe/esthete/EstheteExhibitionServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.blackshoe.esthete;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = EstheteExhibitionServiceApplicationTests.class)
 class EstheteExhibitionServiceApplicationTests {
 
 	@Test
