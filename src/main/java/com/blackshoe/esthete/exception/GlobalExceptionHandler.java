a2e15f9f37391c4cf7755b29e6a5d7ff65636bf3
bbbang105 <2018111366@dgu.ac.kr>
2024-04-29 04:02:08 +0900

    [EST-99] Feat : S3 전역 에러 처리 기능 추가

--- src/main/java/com/blackshoe/esthete/exception/GlobalExceptionHandler.java
+++ src/main/java/com/blackshoe/esthete/exception/GlobalExceptionHandler.java
@@ -11,22 +11,28 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
+    // Token Error
     @ExceptionHandler(TokenException.class)
     public ResponseEntity<ApiResponse<BaseErrorCode>> handleTokenException(TokenException e) {
         TokenErrorResult errorResult = e.getTokenErrorResult();
         return ApiResponse.onFailure(errorResult);
     }
-
+    // User Error
     @ExceptionHandler(UserException.class)
     public ResponseEntity<ApiResponse<BaseErrorCode>> handleUserException(UserException e) {
         UserErrorResult errorResult = e.getUserErrorResult();
         return ApiResponse.onFailure(errorResult);
     }
-
+    // Tag Error
     @ExceptionHandler(TagException.class)
     public ResponseEntity<ApiResponse<BaseErrorCode>> handleUserException(TagException e) {
         TagErrorResult errorResult = e.getTagErrorResult();
+        return ApiResponse.onFailure(errorResult);
+    }
+    // S3 Error
+    @ExceptionHandler(S3Exception.class)
+    public ResponseEntity<ApiResponse<BaseErrorCode>> handleS3Exception(S3Exception e) {
+        S3ErrorResult errorResult = e.getS3ErrorResult();
         return ApiResponse.onFailure(errorResult);
     }
 }
