1bdf5b7ecf6cb5698ef08a17d545a6ebd7ef9ba4
bbbang105 <2018111366@dgu.ac.kr>
2024-06-22 22:15:23 +0900

    [EST-152] Feat : 팔로우 취소 성공 & 실패 Enum 추가

--- src/main/java/com/blackshoe/esthete/exception/MyGalleryErrorResult.java
+++ src/main/java/com/blackshoe/esthete/exception/MyGalleryErrorResult.java
@@ -14,11 +14,13 @@ public enum MyGalleryErrorResult implements BaseErrorCode {
     UNAUTHORIZED_TEMPORARY_EXHIBITION(HttpStatus.UNAUTHORIZED, "401", "사용자의 임시저장 전시회가 아닙니다."),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "400", "올바른 요청이 아닙니다."),
     IS_ALREADY_LIKED(HttpStatus.CONFLICT, "409", "이미 좋아요를 누른 전시회입니다."),
-    IS_ALREADY_NOT_LIKED(HttpStatus.CONFLICT, "409", "이미 좋아요가 없는 전시회입니다."),
+    IS_NOT_LIKED(HttpStatus.CONFLICT, "409", "좋아요를 누르지 않은 전시회입니다."),
     CANNOT_LIKE_ON_OWN_EXHIBITION(HttpStatus.BAD_REQUEST, "400", "본인 전시회에는 좋아요를 누를 수 없습니다."),
     FAIL_TO_GET_FOLLOWS(HttpStatus.NOT_FOUND, "404", "팔로우 목록을 불러오는 데 실패했습니다."),
     CANNOT_FOLLOW_ON_OWN(HttpStatus.BAD_REQUEST, "400", "본인을 팔로우할 수 없습니다."),
     IS_ALREADY_FOLLOWED(HttpStatus.CONFLICT, "409", "이미 팔로우 된 계정입니다."),
+    IS_NOT_FOLLOWED(HttpStatus.CONFLICT, "409", "팔로우하지 않은 계정입니다."),
+    NOT_FOUND_FOLLOWER(HttpStatus.NOT_FOUND, "404", "팔로워를 찾을 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
