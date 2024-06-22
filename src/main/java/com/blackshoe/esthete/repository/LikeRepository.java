94ff27e653ef49e930c88b4af190069993bab412
bbbang105 <2018111366@dgu.ac.kr>
2024-06-22 14:37:05 +0900

    [EST-142] Feat : 댓글 좋아요 취소 API 구현 (DELETE)

--- src/main/java/com/blackshoe/esthete/repository/LikeRepository.java
+++ src/main/java/com/blackshoe/esthete/repository/LikeRepository.java
@@ -4,6 +4,10 @@ import com.blackshoe.esthete.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface LikeRepository extends JpaRepository<Like,Long> {
+    Optional<Like> findByUserIdAndExhibitionId(UUID userId, UUID exhibitionId);
 }
