243465bce580123a098369f1edab7e8ce5e271b4
bbbang105 <2018111366@dgu.ac.kr>
2024-06-22 09:10:23 +0900

    [EST-137] Feat : 임시저장 전시 상세 조회 DTO 추가

--- src/main/java/com/blackshoe/esthete/dto/MyGalleryDto.java
+++ src/main/java/com/blackshoe/esthete/dto/MyGalleryDto.java
@@ -1,7 +1,11 @@
 package com.blackshoe.esthete.dto;
 
+import com.blackshoe.esthete.entity.ExhibitionLocation;
+import com.blackshoe.esthete.entity.Photo;
+import com.blackshoe.esthete.entity.PhotoUrl;
 import com.blackshoe.esthete.entity.TemporaryExhibition;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
@@ -38,6 +42,78 @@ public class MyGalleryDto {
             return temporaryExhibitions.stream()
                     .map(TemporaryExhibitionResponse::of)
                     .collect(Collectors.toList());
+        }
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class TemporaryExhibitionDetailResponse {
+        private String title;
+        private String description;
+        private List<PhotoResponse> photos;
+        private List<String> tags;
+        @JsonProperty("location")
+        private LocationResponse locationResponse;
+
+        public static TemporaryExhibitionDetailResponse of(TemporaryExhibition temporaryExhibition) {
+            return TemporaryExhibitionDetailResponse.builder()
+                    .title(temporaryExhibition.getTitle())
+                    .description(temporaryExhibition.getDescription())
+                    .photos(PhotoResponse.of(temporaryExhibition.getPhotos()))
+                    .tags(EditUserTagsDto.TagList.ofExhibitionTags(temporaryExhibition.getExhibitionTags()))
+                    .locationResponse(LocationResponse.of(temporaryExhibition.getExhibitionLocation()))
+                    .build();
+        }
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class PhotoResponse {
+        private UUID photoId;
+        private UUID filterId;
+        private Float grayScale;
+
+        public static PhotoResponse of(Photo photo) {
+            return PhotoResponse.builder()
+                    .photoId(photo.getPhotoId())
+                    .filterId(photo.getFilterId())
+                    .grayScale(photo.getGrayScale())
+                    .build();
+        }
+
+        public static List<PhotoResponse> of(List<Photo> photos) {
+            return photos.stream()
+                    .map(PhotoResponse::of)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class LocationResponse {
+        private Double longitude;
+        private Double latitude;
+        private String state;
+        private String city;
+        private String town;
+
+        public static LocationResponse of(ExhibitionLocation exhibitionLocation) {
+            return LocationResponse.builder()
+                    .longitude(exhibitionLocation.getLongitude())
+                    .latitude(exhibitionLocation.getLatitude())
+                    .state(exhibitionLocation.getState())
+                    .city(exhibitionLocation.getCity())
+                    .town(exhibitionLocation.getTown())
+                    .build();
         }
     }
 }
