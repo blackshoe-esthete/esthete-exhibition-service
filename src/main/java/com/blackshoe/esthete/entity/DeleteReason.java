012c9a8a2bdd26391348c7dc1cefeeae0d483100
BoyoungH <hyong0903@naver.com>
2024-06-29 00:36:45 +0900

    [EST-156]Add: 회원탈퇴 이유 Enum 변수 추가

---
+++ src/main/java/com/blackshoe/esthete/entity/DeleteReason.java
@@ -0,0 +1,20 @@
+package com.blackshoe.esthete.entity;
+
+public enum DeleteReason {
+    NOT_A_TARGET_USER("서비스 이용 대상이 아님"),
+    TOO_COMPLEX("서비스 이용이 복잡하고 어려움"),
+    SERVICE_ERROR("서비스 장애와 오류 때문에"),
+    NO_INTENTION_AFTER_REFUND("환불 후 이용 의사가 없어서"),
+    REJOINING_AFTER_DELETION("탈퇴 후 신규 가입하기 위함"),
+    OTHER("기타");
+
+    private final String reason;
+
+    DeleteReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+}
