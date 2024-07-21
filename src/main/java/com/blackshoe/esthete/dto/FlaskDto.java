894fdb3120aaa0ad21f16ebb243c6515b81e26aa
bbbang105 <2018111366@dgu.ac.kr>
2024-07-21 10:44:28 +0900

    [EST-157] Feat : Flask 관련 DTO 추가

---
+++ src/main/java/com/blackshoe/esthete/dto/FlaskDto.java
@@ -0,0 +1,53 @@
+package com.blackshoe.esthete.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+public class FlaskDto {
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class UserInfo {
+        private final Long userId;
+        private final List<String> userTagNames;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class ExhibitionInfo {
+        private final Long exhibitionId;
+        private final List<String> exhibitionTagNames;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class RecommendationInfo {
+        private final Long userId;
+        private final List<Recommendation> recommendations;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class Recommendation {
+        private final Long exhibitionId;
+        private final Float weight;
+    }
+}
