f3c2fa915906ac408f54f5ab207f0fa06573dea3
BoyoungH <hyong0903@naver.com>
2024-07-21 15:25:04 +0900

    Feat: KafkaDeletePhotoConsumer 추가

--- src/main/java/com/blackshoe/esthete/exception/KafkaErrorResult.java
+++ src/main/java/com/blackshoe/esthete/exception/KafkaErrorResult.java
@@ -11,7 +11,10 @@ import org.springframework.http.HttpStatus;
 public enum KafkaErrorResult implements BaseErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "404", "사용자를 찾을 수 없습니다."),
     JSON_CONVERSION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"500", "JSON 변환 중 오류가 발생했습니다."),
-    FAIL_TO_SEND_KAFKA(HttpStatus.INTERNAL_SERVER_ERROR,"500", "카프카 전송에 실패했습니다.");
+    FAIL_TO_SEND_KAFKA(HttpStatus.INTERNAL_SERVER_ERROR,"500", "카프카 전송에 실패했습니다."),
+    PHOTO_NOT_FOUND(HttpStatus.NOT_FOUND, "404", "사진을 찾을 수 없습니다."),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "404", "댓글을 찾을 수 없습니다."),
+    ;
 
     private final HttpStatus httpStatus;
     private final String code;
